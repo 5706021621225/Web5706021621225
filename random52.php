c58854318594056fe2937827343b8e2a40856a1f
5706021621225 <5706021621225@fitm.kmutnb.ac.th>
2016-02-19 23:27:51 +0700

    การบ้านสุ่มตัวเลข 1-52 โดยไม่ซ้ำกัน (Update 4)

--- random52.php
+++ random52.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+  <title>สุ่มตัวเลข 1 ถึง 52 โดยไม่ซ้ำกัน</title>
   <meta charset="utf-8">
 </head>
 <body bgcolor="#FFF8DC">
@@ -7,9 +8,8 @@
     $number=array();
     echo "<form>";
     echo "<table align='center' width='350'>";
-    echo "<tr>";
-    echo "<td align='center' colspan='4' bgcolor='#CDB38B'><big>Random ตัวเลข 1 ถึง 52 โดยไม่ซ้ำกัน</big></td>";
-    echo "</tr>";
+    echo "<tr><td align='center' colspan='4' bgcolor='#CDB38B'><big>Random ตัวเลข 1 ถึง 52 โดยไม่ซ้ำกัน</big></td></tr>";
+
     for($n=1;$n<=13;$n++)
     {
       echo "<tr>";
@@ -25,14 +25,10 @@
           }
           else $rand=mt_rand(1,52);
         }
-        if($number[$rand]%2==0)
-          echo "<td align='center' bgcolor='#EECFA1'>";
-        elseif($number[$rand]%3==0)
-          echo "<td align='center' bgcolor='#CDB38B'>";
-        elseif($number[$rand]%7==0)
-          echo "<td align='center' bgcolor='#8B795E'>";
-        else
-            echo "<td align='center' bgcolor='#FFFACD'>";
+        if($number[$rand]%2==0) echo "<td align='center' bgcolor='#EECFA1'>";
+        elseif($number[$rand]%3==0) echo "<td align='center' bgcolor='#CDB38B'>";
+        elseif($number[$rand]%7==0) echo "<td align='center' bgcolor='#8B795E'>";
+        else echo "<td align='center' bgcolor='#FFFACD'>";
         echo "<big>".$number[$rand]."</big>";
         echo "</td>";
       }
